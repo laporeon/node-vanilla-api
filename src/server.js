4fdd75f794059674e5b60f0e465a66c74d7b9d57
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-01 18:57:49 -0300

    feat: create home and movies routes

--- src/server.js
+++ src/server.js
@@ -1,16 +1,46 @@
-import { createServer } from "node:http";
+const { createServer } = require("node:http");
+const { URL } = require("node:url");
+const path = require("node:path");
+const fs = require("node:fs");
+
+const generateResponse = require("./utils/response");
 
 const PORT = 8001;
 
-const server = createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.write(
-    JSON.stringify({
-      message: "Welcome to Node Vanilla API!",
-    })
-  );
+const filePath = path.resolve(__dirname, "data", "movies.json");
 
-  response.end();
+const server = createServer((request, response) => {
+  const { method, url } = request;
+
+  const baseURL = new URL(`http://localhost:${PORT}${url}`);
+
+  const route = baseURL.pathname;
+
+  if (route === "/") {
+    return generateResponse(response, 200, {
+      message: "Welcome to Node Vanilla API!",
+    });
+  }
+
+  if (route.match("/movies") && method === "GET") {
+    const params = route.split("/")[2] || "";
+
+    if (params === "") {
+      const movies = fs.readFileSync(filePath, "utf-8");
+      return generateResponse(response, 200, movies);
+    }
+
+    console.log({ params });
+
+    return generateResponse(response, 200, {
+      message: `Searching for params ${params}`,
+    });
+  }
+
+  // 404 - Route Not Found
+  return generateResponse(response, 404, {
+    error: `Route [${url}] not found!`,
+  });
 });
 
 server.listen(PORT, () => {
