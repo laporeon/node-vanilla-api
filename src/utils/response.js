652b856946830bc92edbebfcc7d3b96f9d2bc1d8
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-05 18:46:24 -0300

    feat: set access control headers

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,4 +1,9 @@
 function generateResponse(response, statusCode, data) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS,PUT,DELETE"
+  );
   response.writeHead(statusCode, { "content-type": "application/json" });
   response.write(JSON.stringify(data));
 
