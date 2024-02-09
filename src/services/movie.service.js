ad87520522171c3983dfa9862011068390672937
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-09 17:55:41 -0300

    feat(services): implement create movie method

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,3 +1,5 @@
+const { randomUUID } = require("node:crypto");
+
 const MovieRepository = require("../repositories/movie.repository");
 
 class MovieService {
@@ -15,6 +17,22 @@ class MovieService {
   async getMovieById(param) {
     // TODO: Error Handler
     const movie = await this.movieRepository.findById(param);
+
+    return movie;
+  }
+
+  async create(body) {
+    // TODO: Error Handler and body validations!
+    const { name, year, director } = body;
+
+    const movie = {
+      id: randomUUID(),
+      name,
+      year,
+      director,
+    };
+
+    await this.movieRepository.create(movie);
 
     return movie;
   }
