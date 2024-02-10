450e713fbe6cf6fe8342e22e89aeebc1fc0c0726
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-10 09:22:18 -0300

    refactor(repository): change file path to new txt file

--- src/repositories/movie.repository.js
+++ src/repositories/movie.repository.js
@@ -3,12 +3,11 @@ const fs = require("node:fs/promises");
 
 class MovieRepository {
   constructor() {
-    this.file = path.resolve(__dirname, "..", "data", "movies.json");
-    this.file2 = path.resolve(__dirname, "..", "data", "movies.txt");
+    this.file = path.resolve(__dirname, "..", "data", "movies.txt");
   }
 
   async get() {
-    const fileContent = await fs.readFile(this.file2, "utf-8");
+    const fileContent = await fs.readFile(this.file, "utf-8");
 
     const data = fileContent.split("\n").splice(1);
 
@@ -38,7 +37,7 @@ class MovieRepository {
     const { id, name, year, director } = movie;
 
     await fs.appendFile(
-      this.file2,
+      this.file,
       `\n${id};${name};${year};${director}`,
       "utf-8"
     );
