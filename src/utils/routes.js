f75d7d4c65cbd22a3f49757d9ed76483ec0ca96b
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-14 16:12:41 -0300

    fix: fix regex for valid routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,6 @@
 const POST_ROUTE = /^\/(movies)\/?$/g;
 const GET_ROUTE =
-  /(?:\/movies\/?)?([0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{12})?/;
+  /^\/movies(\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})?\/?$/;
 
 module.exports = {
   GET_ROUTE,
