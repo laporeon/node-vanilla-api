48e3abced12d934555f9db8d209d0d508887fd29
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-14 13:26:40 -0300

    refactor: change REGEX to accept UUID instead of single id

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,5 +1,6 @@
 const POST_ROUTE = /^\/(movies)\/?$/g;
-const GET_ROUTE = /^\/movies(?:\/(\d+))?\/?$/g;
+const GET_ROUTE =
+  /(?:\/movies\/?)?([0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{12})?/;
 
 module.exports = {
   GET_ROUTE,
