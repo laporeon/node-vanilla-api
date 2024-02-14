852df53fbf4ea52e6f6734cde086877883ad883b
laporeon <34722707+laporeon@users.noreply.github.com>
2024-02-14 16:13:06 -0300

    feat: implement error handler on routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const { URL } = require("node:url");
 
 const MovieController = require("./controllers/movie.controller");
 const generateResponse = require("./utils/response");
+const errorHandler = require("./errors/errorHandler");
 const { GET_ROUTE, POST_ROUTE } = require("./utils/routes");
 
 const PORT = 8001;
@@ -16,39 +17,43 @@ const server = createServer(async (request, response) => {
 
   const route = baseURL.pathname;
 
-  if (route === "/") {
-    return generateResponse(response, 200, {
-      message: "Welcome to Node Vanilla API!",
-    });
-  }
+  try {
+    if (route === "/") {
+      return generateResponse(response, 200, {
+        message: "Welcome to Node Vanilla API!",
+      });
+    }
 
-  if (method === "POST" && route.match(POST_ROUTE)) {
-    return await movieController.createMovie(request);
-  }
+    if (method === "POST" && route.match(POST_ROUTE)) {
+      return await movieController.createMovie(request);
+    }
 
-  if (method === "GET" && route.match(GET_ROUTE)) {
-    const param = request.url.split("/")[2] || "";
-    if (param === "") return await movieController.getAllMovies();
+    if (method === "GET" && route.match(GET_ROUTE)) {
+      const param = request.url.split("/")[2] || "";
+      if (param === "") return await movieController.getAllMovies();
 
-    return await movieController.getMovieById(param);
-  }
+      return await movieController.getMovieById(param);
+    }
 
-  if (method === "PUT" && route.match(GET_ROUTE)) {
+    if (method === "PUT" && route.match(GET_ROUTE)) {
+      return generateResponse(response, 404, {
+        message: "PUT method isn't implemented yet",
+      });
+    }
+
+    if (method === "DELETE" && route.match(GET_ROUTE)) {
+      return generateResponse(response, 404, {
+        message: "DELETE method isn't implemented yet",
+      });
+    }
+
+    // // 404 - Route Not Found
     return generateResponse(response, 404, {
-      message: "PUT method isn't implemented yet",
+      error: `Route [${url}] not found!`,
     });
+  } catch (error) {
+    errorHandler(response, error);
   }
-
-  if (method === "DELETE" && route.match(GET_ROUTE)) {
-    return generateResponse(response, 404, {
-      message: "DELETE method isn't implemented yet",
-    });
-  }
-
-  // 404 - Route Not Found
-  return generateResponse(response, 404, {
-    error: `Route [${url}] not found!`,
-  });
 });
 
 server.listen(PORT, () => {
