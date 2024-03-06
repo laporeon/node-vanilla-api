d7095b197075d8848ed0f6091fb7c493141752e5
laporeon <34722707+laporeon@users.noreply.github.com>
2024-03-06 15:59:01 -0300

    feat(services): create update method

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,33 +1,13 @@
 const { randomUUID } = require("node:crypto");
-const http = require("node:http");
 
 const MovieRepository = require("../repositories/movie.repository");
-const {
-  NotFoundError,
-  RequiredFieldError,
-  InvalidInputError,
-  AlreadyRegisteredError,
-} = require("../errors/errors");
+const { NotFoundError, AlreadyRegisteredError } = require("../errors/errors");
+const Validator = require("../utils/validator");
 
 class MovieService {
   constructor() {
     this.movieRepository = new MovieRepository();
-  }
-
-  async getMovies() {
-    const movies = await this.movieRepository.get();
-
-    return movies;
-  }
-
-  async getMovieById(param) {
-    const movie = await this.movieRepository.findById(param);
-
-    if (!movie) {
-      throw new NotFoundError();
-    }
-
-    return movie;
+    this.validator = new Validator();
   }
 
   async create(request) {
@@ -42,37 +22,12 @@ class MovieService {
         try {
           const parsedBody = JSON.parse(body);
 
+          await this.validator.execute(parsedBody);
+
           const { title, year, genre, duration, ageRating, director } =
             parsedBody;
 
-          if (
-            !title ||
-            !year ||
-            !genre ||
-            !duration ||
-            !ageRating ||
-            !director
-          ) {
-            throw new RequiredFieldError();
-          }
-
-          // TODO: find a better way to validate all the required fields
-
-          if (!Number.isInteger(year)) {
-            throw new InvalidInputError("Year must be type number.");
-          }
-
-          if (!Number.isInteger(ageRating)) {
-            throw new InvalidInputError("Age Rating must be type number.");
-          }
-
-          if (!Number.isInteger(duration)) {
-            throw new InvalidInputError(
-              "Movie duration must be type number and sent in minutes format e.g 146"
-            );
-          }
-
-          const hasMovie = await this.movieRepository.findByTitle(title);
+          const hasMovie = await this.movieRepository.getByTitle(title);
 
           if (hasMovie) {
             throw new AlreadyRegisteredError();
@@ -98,8 +53,69 @@ class MovieService {
     });
   }
 
+  async getMovies() {
+    const movies = await this.movieRepository.get();
+
+    return movies;
+  }
+
+  async getMovieById(param) {
+    const movie = await this.movieRepository.getByID(param);
+
+    if (!movie) {
+      throw new NotFoundError();
+    }
+
+    return movie;
+  }
+
+  async update(request) {
+    return new Promise((resolve, reject) => {
+      let body = "";
+
+      request.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      request.on("end", async () => {
+        try {
+          const id = request.url.split("/")[2] || "";
+
+          const hasMovie = await this.movieRepository.getByID(id);
+
+          if (!hasMovie) {
+            throw new NotFoundError();
+          }
+
+          const parsedBody = JSON.parse(body);
+
+          await this.validator.execute(parsedBody);
+
+          const { title, year, genre, duration, ageRating, director } =
+            parsedBody;
+
+          const updatedMovie = {
+            id,
+            title,
+            year,
+            genre,
+            duration,
+            ageRating,
+            director,
+          };
+
+          await this.movieRepository.update(updatedMovie);
+
+          resolve(updatedMovie);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+  }
+
   async delete(param) {
-    const movie = await this.movieRepository.findById(param);
+    const movie = await this.movieRepository.getByID(param);
 
     if (!movie) {
       throw new NotFoundError();
